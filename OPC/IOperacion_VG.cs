44be6035a2f37f1ee7b5f17ee7b7fd0dc4a2a76f
benyb10 <bryanyb2010@gmail.com>
2025-10-08 10:47:13 -0500

    VITERI GOMEZ 10:45

---
+++ OPC/IOperacion_VG.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// PRINCIPIOS: OCP (Open/Closed Principle) y DIP (Dependency Inversion Principle).
+// - OCP: permite añadir nuevas operaciones (Suma, Resta, ...) sin modificar consumidores.
+// - DIP: los módulos de alto nivel (Calculadora) dependen de esta abstracción. 
+namespace examen01_ti_gomez_viteri.OPC
+{
+    internal class IOperacion_VG
+    {
+    }
+}
